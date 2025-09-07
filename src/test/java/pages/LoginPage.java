011801c97c07da9a12ab45aec3a313a077257880
chocole-24 <lethitan2412001@gmail.com>
2025-09-07 13:35:32 +0700

    revise test file

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,10 +1,8 @@
 package pages;
 
 import base.BasePage;
-import com.fasterxml.jackson.databind.ser.Serializers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class LoginPage extends BasePage {
     public LoginPage(String baseUrl,WebDriver driver) {
--- src/test/java/test/InitializeWebDriver.java
+++ src/test/java/test/InitializeWebDriver.java
@@ -4,8 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.*;
 public class InitializeWebDriver {
     public static WebDriver initializeDriver() {
         ChromeOptions options = new ChromeOptions();
